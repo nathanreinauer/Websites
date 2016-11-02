2dc66045f47f09072f4f198d10220bba47360ce4
Nathan Reinauer <nathanreinauer@gmail.com>
2016-11-01 22:27:50 -0700

    Created database, successfully prints list of businesses to Member page

--- Chamber/Chamber/Controllers/ChamberController.cs
+++ Chamber/Chamber/Controllers/ChamberController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Chamber.Entity;
 
 namespace Chamber.Controllers
 {
@@ -11,6 +12,35 @@ namespace Chamber.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Members()
+        {
+            string memberList = MemberList();
+            ViewBag.Message = memberList;
+
+            return View();
+        }
+
+        public static List<Business> GetBusinesses()
+        {
+            var db = new BusinessesDbEntities();
+            var businesses = db.Businesses.ToList();
+            return businesses;
+        }
+
+        public string MemberList()
+        {
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+
+            var businesses = GetBusinesses();
+
+            foreach (var business in businesses)
+            {
+                sb.Append("<li>" + business.Business1 + "</li>");
+            }
+            
+            return sb.ToString();
         }
     }
 }
